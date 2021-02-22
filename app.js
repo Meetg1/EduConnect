583d604fe8f4642769f63dd018be03ce4c8882aa
meet <meetgajra55@gmail.com>
2021-02-22 22:07:52 +0530

    Added database connection through mongoose

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+//====================DATABASE CONNECTION==========================
+const dbUrl = "mongodb://localhost:27017/test";
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "database connection error:"));
+db.once("open", () => {
+  console.log("Database connected!!");
+});
+//===================================================================
 
 app.get("/results", (req, res) => {
   res.render("results.ejs");
@@ -32,6 +48,8 @@ app.get("/single_material", (req, res) => {
   res.render("single_material.ejs");
 });
 
-app.listen(3000, (err) => {
-  console.log("Server started on port 3000");
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
